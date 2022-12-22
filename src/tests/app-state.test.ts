42cf32163389db2017d9233fda8866291365ec03
youwol-guillaume <greinisch@youwol.com>
2022-12-22 17:30:56 +0100

    ✅ ♻️ app-state.test => make sure to have a unique async execution flow

--- src/tests/app-state.test.ts
+++ src/tests/app-state.test.ts
@@ -1,60 +1,68 @@
 import { AppState } from '../app'
-import { combineLatest } from 'rxjs'
-import { shareReplay, take, tap } from 'rxjs/operators'
+import { combineLatest, of } from 'rxjs'
+import { map, mergeMap, take, tap } from 'rxjs/operators'
 import { MockClient } from './common'
 
-test('AppState test', () => {
+function extractCurrentState(state: AppState) {
+    return (obs) => {
+        return obs.pipe(
+            mergeMap(({ context }) => {
+                return combineLatest([
+                    state.items$,
+                    state.completed$,
+                    state.remaining$,
+                ]).pipe(
+                    take(1),
+                    map(([items, completed, remaining]) => ({
+                        items,
+                        completed,
+                        remaining,
+                        context,
+                    })),
+                )
+            }),
+        )
+    }
+}
+
+// eslint-disable-next-line jest/no-done-callback -- more readable that way
+test('AppState test', (done) => {
     const client = new MockClient({
         items: [{ id: 0, name: 'test-item-0', done: false }],
     })
 
     const state = new AppState({ client })
-    const tested_items$ = state.items$.pipe(
-        shareReplay({ bufferSize: 1, refCount: true }),
-    )
-    const tested_completed$ = state.completed$.pipe(
-        shareReplay({ bufferSize: 1, refCount: true }),
-    )
-    const tested_remaining$ = state.remaining$.pipe(
-        shareReplay({ bufferSize: 1, refCount: true }),
-    )
-    function expectCurrentState(expectCallback) {
-        return combineLatest([
-            tested_items$,
-            tested_completed$,
-            tested_remaining$,
-        ])
-            .pipe(
-                take(1),
-                tap(([items, completed, remaining]) => {
-                    expectCallback({ items, completed, remaining })
-                }),
-            )
-            .subscribe()
-    }
-    expectCurrentState(({ items, completed, remaining }) => {
-        expect(items).toHaveLength(1)
-        expect(completed).toBeFalsy()
-        expect(remaining).toHaveLength(1)
-    })
 
-    const addedItem = state.addItem('item 2')
+    of({ context: {} })
+        .pipe(
+            extractCurrentState(state),
+            tap(({ items, completed, remaining, context }) => {
+                expect(items).toHaveLength(1)
+                expect(completed).toBeFalsy()
+                expect(remaining).toHaveLength(1)
 
-    expectCurrentState(({ items, completed, remaining }) => {
-        expect(items).toHaveLength(2)
-        expect(completed).toBeFalsy()
-        expect(remaining).toHaveLength(2)
-    })
+                context['item'] = state.addItem('item 2')
+            }),
+            extractCurrentState(state),
+            tap(({ items, completed, remaining, context }) => {
+                expect(items).toHaveLength(2)
+                expect(completed).toBeFalsy()
+                expect(remaining).toHaveLength(2)
 
-    state.toggleItem(addedItem.id)
-    expectCurrentState(({ completed, remaining }) => {
-        expect(completed).toBeFalsy()
-        expect(remaining).toHaveLength(1)
-    })
+                state.toggleItem(context['item'].id)
+            }),
+            extractCurrentState(state),
+            tap(({ completed, remaining }) => {
+                expect(completed).toBeFalsy()
+                expect(remaining).toHaveLength(1)
 
-    state.toggleItem(0)
-    expectCurrentState(({ completed, remaining }) => {
-        expect(completed).toBeTruthy()
-        expect(remaining).toHaveLength(0)
-    })
+                state.toggleItem(0)
+            }),
+            extractCurrentState(state),
+            tap(({ completed, remaining }) => {
+                expect(completed).toBeTruthy()
+                expect(remaining).toHaveLength(0)
+            }),
+        )
+        .subscribe(() => done())
 })
