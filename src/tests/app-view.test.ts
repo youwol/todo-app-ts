55272f4b73b682fe35b5bfd31b2cb40222359622
youwol-guillaume <greinisch@youwol.com>
2022-12-23 09:24:13 +0100

    ✅ clean, update & comment tests

--- src/tests/app-view.test.ts
+++ src/tests/app-view.test.ts
@@ -1,8 +1,27 @@
-import { AppState, AppView, ItemsView, ItemView } from '../app'
-import { MockClient } from './common'
+import {
+    AppState,
+    AppView,
+    FooterView,
+    ItemsView,
+    ItemView,
+    NewItemView,
+} from '../app'
+import { extractActualProperties, MockClient } from './common'
 import { render } from '@youwol/flux-view'
 
-test('AppView test', () => {
+beforeEach(() => {
+    document.body.textContent = ''
+})
+
+test('on-load does insert the view in the DOM', async () => {
+    await import('../app/on-load')
+    const appView: AppView & HTMLElement = document.querySelector(
+        `.${AppView.ClassName}`,
+    )
+    expect(appView).toBeTruthy()
+})
+
+test('views with a complete scenario', async () => {
     const client = new MockClient({
         items: [
             { id: 0, name: 'test-item-0', done: false },
@@ -12,8 +31,12 @@ test('AppView test', () => {
 
     const state = new AppState({ client })
     const view = new AppView({ state })
+
+    //----------------
+    // Make sure the root view (AppView) is displayed
+    //----------------
+
     const div = render(view)
-    document.body.textContent = ''
     document.body.appendChild(div)
     const appView: AppView & HTMLElement = document.querySelector(
         `.${AppView.ClassName}`,
@@ -21,6 +44,11 @@ test('AppView test', () => {
 
     expect(appView).toBeTruthy()
     expect(appView.filterMode$.value).toBe('All')
+
+    //----------------
+    // Test the view of items container
+    //----------------
+
     const itemsViewContainer: ItemsView & HTMLElement = document.querySelector(
         `.${ItemsView.ClassName}`,
     )
@@ -37,10 +65,18 @@ test('AppView test', () => {
         done: false,
     })
 
+    //----------------
+    // Test toggle on first item using the state
+    //----------------
+
     state.toggleItem(0)
     itemsView = document.querySelectorAll(`.${ItemView.ClassName}`)
     expect([...itemsView]).toHaveLength(2)
     expect(itemsView[0].item.done).toBeTruthy()
+
+    //----------------
+    // Test renaming the first item
+    //----------------
 
     const item = itemsView[0]
     const innerItemView = item.querySelector('.presentation-view')
@@ -48,4 +84,90 @@ test('AppView test', () => {
     innerItemView.dispatchEvent(new Event('dblclick', { bubbles: true }))
     const editionView = item.querySelector('input')
     expect(editionView).toBeTruthy()
+    editionView.dispatchEvent(new Event('click', { bubbles: true }))
+    editionView.dispatchEvent(new KeyboardEvent('keypress', { key: 'f' }))
+    editionView.dispatchEvent(new KeyboardEvent('keypress', { key: 'o' }))
+    editionView.dispatchEvent(new KeyboardEvent('keypress', { key: 'o' }))
+    editionView.value = 'foo'
+    editionView.dispatchEvent(new KeyboardEvent('keypress', { key: 'Enter' }))
+
+    const s0 = await extractActualProperties(state)
+    expect(s0.items[0].name).toBe('foo')
+
+    //----------------
+    // Test toggle on first item using the view
+    //----------------
+
+    const toggleFirstItemView = item.querySelector('.item-view-toggle')
+    expect(toggleFirstItemView).toBeTruthy()
+    toggleFirstItemView.dispatchEvent(new Event('click', { bubbles: true }))
+    const s1 = await extractActualProperties(state)
+    expect(s1.items.done).toBeFalsy()
+
+    //----------------
+    // Test delete the first item
+    //----------------
+
+    const deleteFirstItemView = item.querySelector('.item-view-remove')
+    deleteFirstItemView.dispatchEvent(new Event('click', { bubbles: true }))
+    itemsView = document.querySelectorAll(`.${ItemView.ClassName}`)
+    expect([...itemsView]).toHaveLength(1)
+
+    //----------------
+    // Test create a new item
+    //----------------
+
+    const newItemView: NewItemView & HTMLElement = document.querySelector(
+        `.${NewItemView.ClassName}`,
+    )
+    expect(newItemView).toBeTruthy()
+    const editionNewItem = newItemView.querySelector('input')
+    expect(editionNewItem).toBeTruthy()
+    editionNewItem.dispatchEvent(new KeyboardEvent('keypress', { key: 'b' }))
+    editionNewItem.dispatchEvent(new KeyboardEvent('keypress', { key: 'a' }))
+    editionNewItem.dispatchEvent(new KeyboardEvent('keypress', { key: 'r' }))
+    editionNewItem.value = 'bar'
+    editionNewItem.dispatchEvent(
+        new KeyboardEvent('keypress', { key: 'Enter' }),
+    )
+
+    const s2 = await extractActualProperties(state)
+    expect(s2.items).toHaveLength(2)
+    expect(s2.items[1].name).toBe('bar')
+
+    //----------------
+    // Test the footer view & its various display modes
+    //----------------
+
+    const footerView: FooterView & HTMLElement = document.querySelector(
+        `.${FooterView.ClassName}`,
+    )
+    expect(footerView).toBeTruthy()
+
+    const displayCompletedToggleView = footerView.querySelector(`.Completed`)
+    displayCompletedToggleView.dispatchEvent(
+        new Event('click', { bubbles: true }),
+    )
+    itemsView = document.querySelectorAll(`.${ItemView.ClassName}`)
+    expect([...itemsView]).toHaveLength(1)
+
+    const displayActiveToggleView = document.querySelector(`.Active`)
+    displayActiveToggleView.dispatchEvent(new Event('click', { bubbles: true }))
+    itemsView = document.querySelectorAll(`.${ItemView.ClassName}`)
+    expect([...itemsView]).toHaveLength(1)
+
+    const displayAllToggleView = document.querySelector(`.All`)
+    displayAllToggleView.dispatchEvent(new Event('click', { bubbles: true }))
+    itemsView = document.querySelectorAll(`.${ItemView.ClassName}`)
+    expect([...itemsView]).toHaveLength(2)
+
+    //----------------
+    // Test toggle all button
+    //----------------
+
+    const toggleAll = document.querySelector('.new-item-view-toggle-all')
+    toggleAll.dispatchEvent(new Event('click', { bubbles: true }))
+    displayActiveToggleView.dispatchEvent(new Event('click', { bubbles: true }))
+    itemsView = document.querySelectorAll(`.${ItemView.ClassName}`)
+    expect([...itemsView]).toHaveLength(0)
 })
